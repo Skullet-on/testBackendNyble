90fdd11159750a77a5c8d5e0b6db94567b88aa62
unknown <skullet-on@mail.ru>
2019-06-27 17:22:14 +0300

    changed var on const

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
 const dotenv = require('dotenv');
 dotenv.config();
 const port = process.env.PORT || 3000;
